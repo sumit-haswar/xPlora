58ee22c74c7bc9ced0bd525fd3fb7ff15aee4f92
Sumit Haswar <sumithaswar@gmail.com>
2017-02-04 21:23:02 -0800

    Initial solution commit with projects
    Initial solution commit with UI, google news crawler, core app and
    CSharpTest.Net.Collections projects

---
+++ xPlora.GoogleNewsCrawler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xPlora.GoogleNewsCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
